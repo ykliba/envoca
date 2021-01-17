5e2969ea61ba7e614fe4ff09375878aba54cc39b
ykliba <mediaboxing2@gmail.com>
2021-01-17 09:20:34 +0900

    api controller

--- backend/app/Http/Controllers/Auth/Api/RegisterController.php
+++ backend/app/Http/Controllers/Auth/Api/RegisterController.php
@@ -1,44 +1,30 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Auth\Api;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\{Validator, Hash, DB};
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
     use RegistersUsers;
 
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function register(Request $request)
     {
-        $this->middleware('guest');
+        // リクエストを検証します。
+        $this->validator($request->all())->validate();
+
+        // ユーザーの作成とトークンの作成します
+        $data = DB::transaction(function () use ($request) {
+            $user = $this->create($request->all());
+            $token = $user->createToken($request->device_nam)->plainTextToken;
+            return json_encode(['token' => $token, 'user' => $user]);
+        });
+        // userとtokenのjsonを返却
+        return response($data);
     }
 
     /**
@@ -53,6 +39,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'device_name' => ['required']
         ]);
     }
 
@@ -60,7 +47,7 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\Models\User
+     * @return \App\User
      */
     protected function create(array $data)
     {
