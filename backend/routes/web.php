6245e8e8d63cc48c99cface63193d4e7674c1864
ykliba <mediaboxing2@gmail.com>
2021-01-22 09:42:37 +0900

    Update web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
