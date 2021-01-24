eb53f5e594c1846aa01bbb8a60fba67c84a5d001
ykliba <mediaboxing2@gmail.com>
2021-01-24 09:58:08 +0900

    store作成

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -3,6 +3,8 @@ import Vue from 'vue'
 import router from './router'
 // ルートコンポーネントをインポートする
 import App from './App.vue'
+
+import store from './store'
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -36,6 +38,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
     router, // ルーティングの定義を読み込む
+    store,
     components: { App }, // ルートコンポーネントの使用を宣言する
     template: '<App />' // ルートコンポーネントを描画する
 })
